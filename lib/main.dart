cd25741a747ac90463fd853d0a0c925d61f63b4c
Yuga Extrapurpose <extrapurpose.2024@gmail.com>
2026-03-02 20:35:47 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Uzhavar App',
+      title: 'Agrio',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
